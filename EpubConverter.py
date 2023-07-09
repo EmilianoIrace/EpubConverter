ef77b1ded04a5f18e27a3abfc6bdebe0c06e07ae
EmilianoIrace <iracemiliano@gmail.com>
2023-07-09 12:47:25 +0200

    CMD version

--- EpubConverter.py
+++ EpubConverter.py
@@ -5,9 +5,12 @@
 #write the folloing pandoc command in the terminal changing filename
 """
 -i filename -M document-css=false --extract-media .\new\ -o .\new\NewBook.md
+after that
+python modified_EpubConverter.py NewBook.md
 """
 
 
+import sys
 import re
 
 def replace_with_whitespace(text):
@@ -21,18 +24,20 @@ def replace_with_whitespace(text):
     pattern = r'\(#.*?\)'
     
     # Replace the matched text with a white space
-    replaced_text = re.sub(pattern, ' ', text)
+    replaced_text = re.sub(pattern, ' ', replaced_text)
 
     # Define the regular expression pattern to match text inside square brackets []
     pattern = r'\[\s+\]'
     
     # Replace the matched text with a white space
-    replaced_text = re.sub(pattern, ' ', text)
+    replaced_text = re.sub(pattern, ' ', replaced_text)
     
     return replaced_text
 
-# Read the input text from a .md file with specified encoding
-filename = "new\NewBook.md"
+# Get the filename from the command line argument
+filename = sys.argv[1]
+
+# Read the input text from the specified file with specified encoding
 with open(filename, 'r', encoding='utf-8') as file:
     input_text = file.read()
 
